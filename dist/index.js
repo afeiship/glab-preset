5cc33fe6ed82d603e861fa00cd5cc98fb4ed7e44
afeiship <1290657123@qq.com>
2020-04-15 14:00:43 +0800

    fix: version

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,8 @@
 /*!
  * name: @feizheng/gulp-pkg-header
  * url: https://github.com/afeiship/gulp-pkg-header
- * version: 1.0.0
+ * version: 1.0.4
  * license: MIT
  */
 
-var header=require("gulp-header"),path=require("path"),nx=require("@feizheng/next-js-core2"),appPath=require("app-root-path").path,pkg=require(path.join(appPath,"./package.json")),DEFAULT_FORMAT="yyyy-mm-dd HH:MM:ss",dateFormat=require("dateformat");require("@feizheng/next-nice-comments");var niceComments=nx.niceComments(["name: <%= pkg.name %>","description: <%= pkg.description %>","url: <%= pkg.homepage %>","version: <%= pkg.version %>","date: "+dateFormat(new Date,DEFAULT_FORMAT),"license: <%= pkg.license %>"],"js");module.exports=function(){return header(niceComments,{pkg:pkg})};
+var header=require("gulp-header"),path=require("path"),nx=require("@feizheng/next-js-core2"),appPath=require("app-root-path").path,pkg=require(path.join(appPath,"./package.json"));require("@feizheng/next-nice-comments");var niceComments=nx.niceComments(["name: <%= pkg.name %>","description: <%= pkg.description %>","homepage: <%= pkg.homepage %>","version: <%= pkg.version %>","date: "+(new Date).toISOString(),"license: <%= pkg.license %>"],"js");module.exports=function(){return header(niceComments,{pkg:pkg})};
