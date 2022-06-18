422710d503f0f6a4ba55bedf1d51f60153b7b017
afeiship <1290657123@qq.com>
2022-06-18 23:16:20 +0800

    feat: preset for react-oss

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ import click
 
 @click.command()
 @click.option('--project_id', prompt=True, required=True, type=int)
+@click.option('--yarn_registry', prompt=True, default=os.getenv('ALO7_YARN_REGISTRY'))
 @click.option('--alibabacloud_access_key_id', prompt=True, default=os.getenv('ALIBABACLOUD_ACCESS_KEY_ID'))
 @click.option('--alibabacloud_access_key_secret', prompt=True, default=os.getenv('ALIBABACLOUD_ACCESS_KEY_SECRET'))
 @click.option('--alibabacloud_region_id', prompt=True, default=os.getenv('ALIBABACLOUD_REGION_ID'))
@@ -13,8 +14,8 @@ def cli(**kwargs):
   gl = gitlab.Gitlab(url="https://git.saybot.net/", private_token=os.getenv('GITLAB_TOKEN'))
   prj = gl.projects.get(kwargs['project_id'])
 
-  prj.variables.create({'key': 'YARN_REGISTRY', 'value': os.getenv('ALO7_YARN_REGISTRY')})
   envs = [
+    {'key': 'YARN_REGISTRY', 'value': kwargs['yarn_registry']},
     {'key': 'ALIBABACLOUD_ACCESS_KEY_ID', 'value': kwargs['alibabacloud_access_key_id']},
     {'key': 'ALIBABACLOUD_ACCESS_KEY_SECRET', 'value': kwargs['alibabacloud_access_key_secret']},
     {'key': 'ALIBABACLOUD_REGION_ID', 'value': kwargs['alibabacloud_region_id']},
