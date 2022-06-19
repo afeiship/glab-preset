f361e841a66f26f0747b5ccab85eef2c2a20496c
afeiship <1290657123@qq.com>
2022-06-19 08:51:54 +0800

    fix: lazy imporr

--- glab_preset/main.py
+++ glab_preset/main.py
@@ -1,12 +1,11 @@
 import click
 
-from glab_preset.presets.react_oss import react_oss
-
 
 @click.command()
 @click.option('--type', prompt=True, type=click.Choice(['react_oss']), default='react_oss')
 def cli(**kwargs):
   if 'react_oss' == kwargs.get('type'):
+    from glab_preset.presets.react_oss import react_oss
     react_oss()
   else:
     click.echo('Not support.')
