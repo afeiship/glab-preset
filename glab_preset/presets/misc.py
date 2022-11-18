13da3e7f250bf0951feac0c468a15f8c94951184
afeiship <1290657123@qq.com>
2022-11-18 21:41:19 +0800

    fix: create_or update err

--- glab_preset/presets/misc.py
+++ glab_preset/presets/misc.py
@@ -1,7 +1,16 @@
+import gitlab
+
+"""
+This is a sample preset for creating or updating project variables.
+注意：`prj.variables.get(var_name)` 这个 `api` 如果 `var_name` 不存在会直接报错
+"""
+
+
 def create_or_update(prj, item):
-  item_var = prj.variables.get(item['key'])
-  if item_var:
-    item_var.value = item['value']
+  var_name = item.get('key')
+  try:
+    item_var = prj.variables.get(var_name)
+    item_var.value = item.get('value')
     item_var.save()
-  else:
+  except gitlab.exceptions.GitlabGetError:
     prj.variables.create(item)
