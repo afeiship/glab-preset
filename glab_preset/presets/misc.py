628826e20049298538a8d5ec08d299042e6ba2e1
afeiship <1290657123@qq.com>
2022-11-19 08:41:34 +0800

    feat: add version option

--- glab_preset/presets/misc.py
+++ glab_preset/presets/misc.py
@@ -1,4 +1,7 @@
+import re
+
 import gitlab
+import os
 
 """
 This is a sample preset for creating or updating project variables.
@@ -14,3 +17,13 @@ def create_or_update(prj, item):
     item_var.save()
   except gitlab.exceptions.GitlabGetError:
     prj.variables.create(item)
+
+
+def get_version_from_pyproject():
+  pyproject_toml = open('./pyproject.toml', 'r')
+  version_re = r'version = "(.*)"'
+  matches = re.findall(version_re, pyproject_toml.read())
+  if len(matches) > 0:
+    return matches[0]
+  else:
+    return '0.0.0'
